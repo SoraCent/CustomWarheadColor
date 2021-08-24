873e5b0cc286b2121238f288f2516dc17bc46907
SoraCent <Christofgmur@gmail.com>
2021-08-24 22:37:16 +0200

    Changed Config Description

--- CustomWarheadColor/Config.cs
+++ CustomWarheadColor/Config.cs
@@ -14,12 +14,13 @@ namespace CustomWarheadColor
 
         [Description("Activate this if you want that the Light is getting of after Warhead detonation | Default = true")]
         public bool EnableNoLightAfterDetonate { get; set; } = true;
+        [Description("Sets how long the Lights are off in Seconds")]
         public float NoLightAfterDetonateDuration { get; set; } = 60f;
         
         [Description("Activate this if you want Custom Lightcolor after the Warhead detonated | Default = false")]
         public bool EnableCustomLightColorAfterDetonate { get; set; } = false;
         
-        [Description("Light Color R = Red, G = Green, B = Blue from 0-255 | Default 0,0,0 = Black")]
+        [Description("Light Color R = Red, G = Green, B = Blue from 0-255 | Default 0,0,0 = Black - looks like the light is off")]
         public int WarheadColorAfterDetonationR { get; set; } = 0;
         public int WarheadColorAfterDetonationG { get; set; } = 0;
         public int WarheadColorAfterDetonationB { get; set; } = 0;
